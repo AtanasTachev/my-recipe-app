3060808de02e83065d88e0078cb8082150f9680f
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-04-09 17:22:13 +0300

    fix home-page rendering

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
+import { UserModule } from '../user/user.module';
 
 
 
@@ -9,7 +10,8 @@ import { HomePageComponent } from './home-page/home-page.component';
     HomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    UserModule
   ],
   exports: [
     HomePageComponent
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './profile/profile.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,9 @@ import { ProfileComponent } from './profile/profile.component';
   exports: [
     UserListComponent,
     ProfileComponent
+  ],
+  providers: [
+    UserService
   ]
 })
 export class UserModule { }
